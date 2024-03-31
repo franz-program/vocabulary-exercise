8ef7188ff3580130d8fb32dca428bf882413c47c
Francesco Urdih <a12232672@unet.univie.ac.at>
2024-03-31 23:41:42 +0200

    added functioning for one word

--- src/app/word-displayer/word-displayer.component.ts
+++ src/app/word-displayer/word-displayer.component.ts
@@ -1,13 +1,49 @@
 import {Component, Input} from '@angular/core';
 import {Word} from "../../models/word";
+import {NgForOf, NgIf} from "@angular/common";
+import {ButtonModule} from "primeng/button";
+import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
 
 @Component({
   selector: 'app-word-displayer',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    ButtonModule,
+    NgIf,
+    FormsModule,
+    InputTextModule
+  ],
   templateUrl: './word-displayer.component.html',
   styleUrl: './word-displayer.component.css'
 })
 export class WordDisplayerComponent {
   @Input() word: Word | undefined;
+  solutionsRevealed: boolean = false;
+  userTranslations: string[] = [];
+  userResults: boolean[] = [];
+
+  checkTranslations() {
+    if(this.word === undefined)
+      return;
+    let translations = Object.assign([], this.word.to);
+    for(let i = 0; i < this.userTranslations.length; i++)
+      if(translations.includes(this.userTranslations[i])){
+        this.userResults[i] = true;
+        translations.splice(i, 1);
+      } else
+        this.userResults[i] = false;
+
+  }
+
+  revealSolutions(){
+    if(this.word === undefined)
+      return;
+    this.userTranslations = Object.assign([], this.word.to);
+  }
+
+  loadNext(){
+
+  }
 }
